e1b1d1d38d069b8f33f2010e9ecac03a9a2082dd
A Jauzee <a.jauzee@gmail.com>
2021-03-14 23:07:49 +0300

    Declare basic colors palette

---
+++ quiz/Sources/Library/ColorPalette.swift
@@ -0,0 +1,53 @@
+//
+//
+//  quiz
+//  
+//  Created on 08.03.2021
+//  
+//  
+
+import SwiftUI
+
+enum Colors {
+
+    static var green: Color {
+        Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+    }
+
+    static var blue: Color {
+        Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
+    }
+
+    static var red: Color {
+        Color(#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1))
+    }
+
+    static var selectionBackground: Color {
+        Color(#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+    }
+
+    static var text: Color {
+        Color(.label)
+    }
+
+    static var secondaryText: Color {
+        Color(.secondaryLabel)
+    }
+
+    static var tertiaryText: Color {
+        Color(.tertiaryLabel)
+    }
+
+    static var background: Color {
+        Color(.systemBackground)
+    }
+
+    static var secondaryBackground: Color {
+        Color(.secondarySystemBackground)
+    }
+
+    static var tertiaryBackground: Color {
+        Color(.tertiarySystemBackground)
+    }
+
+}
